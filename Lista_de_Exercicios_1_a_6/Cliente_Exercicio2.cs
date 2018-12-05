fbecb55deaf74a1b5b176da11caaa9cdba8320de
Carlos Daniel <45631849+CarlossDaniell@users.noreply.github.com>
2018-12-05 12:56:22 -0200

    Add files via upload

---
+++ Lista_de_Exercicios_1_a_6/Cliente_Exercicio2.cs
@@ -0,0 +1,57 @@
+﻿namespace Lista_de_Exercicios_1_a_6
+{
+    internal class Cliente_Exercicio2
+    {
+        //Atributos
+        private int saldo;
+        private int conta;
+        private int totalCobrancas;
+        private int totalCredito;
+        private int limiteCredito;
+
+        //Construtor padrão
+        //Construtor utiliza o mesmo nome da classe
+        public Cliente_Exercicio2()
+        {
+            //Aqui dentro faremos a inicialização dos atributos da classe
+
+            saldo = 0;
+            conta = 0;
+            totalCobrancas = 0;
+            totalCredito = 0;
+            limiteCredito = 0;
+        }
+
+        //Construtor com valores do usuário
+        //Utiliza o mesmo nome da classe
+
+        public Cliente_Exercicio2(int saldo, int conta, int totalCobrancas, int totalCredito, int limiteCredito)
+        {
+            this.saldo = saldo;
+            this.conta = conta;
+            this.totalCobrancas = totalCobrancas;
+            this.totalCredito = totalCredito;
+            this.limiteCredito = limiteCredito;
+        }
+
+        //Método calcular novo saldo
+        public int calcular_novo_saldo()
+        {
+            return this.saldo + this.totalCobrancas - this.totalCredito;
+        }
+
+
+        //Método atualizar saldo
+        public void atualizar_saldo()
+        {
+            this.saldo = calcular_novo_saldo();
+        }
+
+
+        //Verificar limite ultrapassado
+        public bool verificar_limite()
+        {
+            return this.saldo > this.limiteCredito;
+        }
+    }
+}
---
+++ Lista_de_Exercicios_1_a_6/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associada a um assembly.
+[assembly: AssemblyTitle("Lista_de_Exercicios_1_a_6")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lista_de_Exercicios_1_a_6")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("82813ae0-973c-4f8c-ad39-0afeb2fa2f20")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// utilizando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
